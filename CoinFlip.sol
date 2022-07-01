361fd9da741f66d38278975adcab2b31b2982c99
DamianCaine <iamdreals@gmail.com>
2022-07-01 19:22:13 +0530

    change

--- CoinFlip.sol
+++ CoinFlip.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: Unlicensed
 
-import '@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol';
-import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
 import 'hardhat/console.sol';
 pragma solidity ^0.8.7;
 
